1a0f9eeeaea6ea6b5bcad90c3db3f064e6205a73
Azaz-Ali <azaz.ali1706@gmail.com>
2024-03-23 19:38:31 +0530

    config committed

--- CRUD-Basics/db.js
+++ CRUD-Basics/db.js
@@ -1,10 +1,11 @@
 const mongoose= require('mongoose')
 //define mongodb connection  url
-const mongoURL= 'mongodb+srv://Azaz2642:Qwerty123@cluster-1.ndlz7ro.mongodb.net/hotel'
+require('dotenv').config()
 
+const MONGODB_URL= process.env.Mongo_URL
 //setup mongodb connection
 
-mongoose.connect(mongoURL)
+mongoose.connect(MONGODB_URL)
 const db= mongoose.connection;
 
 db.on('connected', ()=>{
--- CRUD-Basics/index.js
+++ CRUD-Basics/index.js
@@ -1,16 +1,30 @@
-const express= require('express')
-const app= express()
-const db= require('./db')
-const bodyParser= require('body-parser')
-app.use(bodyParser.json())
+const express = require('express');
+const app = express();
+const db = require('./db');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+const PORT = process.env.PORT || 6000;
 
 /******************Person*************************** */
-//Import the person routes
-const personRoutes= require('./routes/personRoutes')
-//Use the person router
-app.use('/person', personRoutes)
-/*******************menu****************************** */
-/*******Saving menu to the db */
-const menuRoutes= require('./routes/menuRoutes')
-app.use('/menu', menuRoutes)
-app.listen(6000)
+// Import the person routes
+const personRoutes = require('./routes/personRoutes');
+// Use the person router
+app.use('/person', personRoutes);
+
+/*******************Menu****************************** */
+// Import menu routes
+const menuRoutes = require('./routes/menuRoutes');
+app.use('/menu', menuRoutes);
+
+// Body parser middleware
+app.use(bodyParser.json());
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
