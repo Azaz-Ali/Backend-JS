4ed5e00d58900e07b12d9503354306df96cf3a82
Azaz-Ali <azaz.ali1706@gmail.com>
2024-03-27 20:30:38 +0530

    auth added

--- CRUD-Basics/index.js
+++ CRUD-Basics/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
-const Person= require('./models/person')
+const passport= require('./auth')
 const bodyParser = require('body-parser');
 require('dotenv').config()
-const passport= require('passport')
-const LocalStrategy= require('passport-local').Strategy;
+
 const PORT = process.env.PORT || 6000;
 
 // Body parser middleware
@@ -18,36 +17,20 @@ const logRequest= (req, res, next)=>{
 }
 app.use(logRequest)
 
-/**********Authentication using Passport******* */
-passport.use(new LocalStrategy(async (username, password, done)=>{
-  //authentication logic here
-  try{
-       console.log("received credentials", username, password);
-       const user= await Person.findOne({username: username})
-       //done takes three parameters (error, user, info)
-    
-       if(!user) return done(null, false, {msg:"incorrect username"})//user not exist
-       const isRightPassword= user.password===password?true:false
-       //is password is right of that user
-       if(isRightPassword) return done(null, user)
-       else return done(null, false, {msg:"incorrect password"})
-  } catch(error){
-       return done(error)
-  }
-}))
 
-//Now use this authentication as a middleware
+
+//Now initialize the passport
 app.use(passport.initialize())
 //auth middleware
 const localAuthMiddleware= passport.authenticate('local', {session:false})
-app.get('/', localAuthMiddleware, (req, res)=>{
+app.get('/', (req, res)=>{
   res.send("Welcome to the Backend Course")
 })
 /******************Person*************************** */
 // Import the person routes
 const personRoutes = require('./routes/personRoutes');
 // Use the person router
-app.use('/person', personRoutes);
+app.use('/person',localAuthMiddleware, personRoutes);
 
 /*******************Menu****************************** */
 // Import menu routes
--- CRUD-Basics/models/person.js
+++ CRUD-Basics/models/person.js
@@ -1,4 +1,5 @@
 const mongoose= require('mongoose')
+const bcrypt= require('bcrypt')
 const personSchema= new mongoose.Schema({
     name:{
         type:String,
@@ -32,6 +33,38 @@ const personSchema= new mongoose.Schema({
         required: true
     }
 })
+
+//bcrypt
+personSchema.pre('save', async function(next){
+    
+        const person= this
+        console.log(person);
+        //if data is modified then no need to hash the password as already hashed
+        if(!person.isModified('password')) return next()
+        //if new data is created then we need to hash the password with salt
+
+    try {
+        //hash password generation
+        const salt= await bcrypt.genSalt(8)
+        // hash password
+        const hashedPassword= await bcrypt.hash(person.password, salt);
+        person.password= hashedPassword
+        next()
+    } catch (error) {
+        return next(error)
+    }
+})
+
+//Compare login password with original password; extract salt from original and add it to login pass then hash it then compare it
+  personSchema.methods.comparePassword= async function(candidatePassword){
+    try {
+        //comapre the provided password with the orginal hashed+salt password
+        const isMatch= await bcrypt.compare(candidatePassword, this.password)
+        return isMatch
+    } catch (error) {
+        throw error
+    }
+  }
 //Creation of model
 const Person= mongoose.model('Person', personSchema)
 module.exports= Person
