dd1e39c72f85a00f663e37aab158d4c4e6522f3d
Azaz-Ali <azaz.ali1706@gmail.com>
2024-03-29 11:34:49 +0530

    node modules

--- coursePurchase/routes/adminRoutes.js
+++ coursePurchase/routes/adminRoutes.js
@@ -2,3 +2,20 @@ const express= require('express')
 const router= express.Router()
 const adminMiddleware = require("../middleware/adminMiddleware");
 const Admin= require('./../models/adminModel');
+
+
+// Admin Routes
+app.post('/signup', (req, res) => {
+    // Implement admin signup logic
+});
+
+app.post('/courses', adminMiddleware, (req, res) => {
+    // Implement course creation logic
+});
+
+app.get('/courses', adminMiddleware, (req, res) => {
+    // Implement fetching all courses logic
+});
+
+
+module.exports = router;
